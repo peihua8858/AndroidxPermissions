d65f02a50d55a548dede876576b467aa3cd6b402
dingpeihua <dph01953558@alibaba-inc.com>
2025-03-03 14:24:01 +0800

    优化代码

--- app/src/main/java/com/peihua8858/permissions/MainActivity.kt
+++ app/src/main/java/com/peihua8858/permissions/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.appcompat.app.AlertDialog
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
@@ -12,21 +13,30 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.AlertDialogDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.lifecycle.lifecycleScope
 import com.fz.toast.showToast
 import com.peihua8858.permissions.core.requestPermission
 import com.peihua8858.permissions.core.requestPermissions
 import com.peihua8858.permissions.theme.ComposeDemoTheme
+import com.peihua8858.permissions.theme.MarketFontFamily
 import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
@@ -47,6 +57,7 @@ class MainActivity : ComponentActivity() {
                             title = {
                                 Text(
                                     text = "Demo",
+                                    fontFamily = MarketFontFamily.NotoSansSc500,
                                     textAlign = TextAlign.Center,
                                     modifier = Modifier
                                         .wrapContentWidth(Alignment.CenterHorizontally) // 水平居中
@@ -58,10 +69,14 @@ class MainActivity : ComponentActivity() {
                     Box(modifier = Modifier.fillMaxSize()) {
                         Greeting("Android",
                             Modifier
+                                .align(Alignment.Center)
                                 .padding(innerPadding)
                                 .clickable {
                                     lifecycleScope.launch {
-                                        requestPermission(Manifest.permission.SYSTEM_ALERT_WINDOW) {
+                                        requestPermissions(
+                                            Manifest.permission.READ_MEDIA_IMAGES,
+                                            Manifest.permission.CAMERA
+                                        ) {
                                             onGranted {
                                                 // 权限已授予
                                                 Log.d("MainActivity>>>", "权限已授予")
@@ -74,7 +89,8 @@ class MainActivity : ComponentActivity() {
                                             }
                                         }
                                     }
-                                })
+                                }
+                        )
                     }
                 }
 
@@ -88,6 +104,7 @@ class MainActivity : ComponentActivity() {
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
         text = "Hello $name!",
+        fontFamily = MarketFontFamily.NotoSansSc500,
         modifier = modifier
     )
 }
